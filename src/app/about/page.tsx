862e480ec1949e43c694597ef3d5689f049e24e4
mlazzarini <margherita.lazzarini@gmail.com>
2024-01-24 13:28:41 +0100

    Add contentlayer to render markdown

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,44 @@
+import { allPosts, Post } from 'contentlayer/generated'
+import { compareDesc, format, parseISO } from 'date-fns'
+import Link from 'next/link'
+
+function PostCard(post: Post) {
+  return (
+    <div className="mb-8">
+      <h2 className="mb-1 text-xl">
+        <Link
+          href={post.url}
+          className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+          {post.title}
+        </Link>
+      </h2>
+      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+        {format(parseISO(post.date), 'LLLL d, yyyy')}
+      </time>
+      <div
+        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
+        dangerouslySetInnerHTML={{ __html: post.body.html }}
+      />
+    </div>
+  )
+}
+
 export default function About() {
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  )
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       I am the about page
+      <div className="mx-auto max-w-xl py-8">
+        <h1 className="mb-8 text-center text-2xl font-black">
+          Next.js + Contentlayer Example
+        </h1>
+        {posts.map((post, idx) => (
+          <PostCard key={idx} {...post} />
+        ))}
+      </div>
     </main>
   )
 }
