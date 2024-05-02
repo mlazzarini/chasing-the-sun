0670dee6025d7cab77abcea0b7f48cba9ed0c456
mlazzarini <margherita.lazzarini@gmail.com>
2024-05-02 09:33:43 -0500

    First post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,6 @@ export default function Home() {
   const posts = allPosts.sort((a, b) =>
     compareDesc(new Date(a.date), new Date(b.date))
   )
-  // 1714428000000 is timestamp for 30 Apr. 2024 (midnight)
-  const showCountdown = useMemo(() => new Date().getTime() < 1714428000000, [])
 
   return (
     <main className="flex min-h-screen flex-col items-center">
@@ -19,19 +17,15 @@ export default function Home() {
         <WorldMap />
       </div>
       <div className="lg:w-1/2 md:w-4/5 sm:w-4/5 mx-auto p-8">
-        {showCountdown ? (
-          <Countdown />
-        ) : (
-          <>
-            <h1
-              className={`${amaticSC.className} mb-8 text-center font-bold text-4xl text-[#FF4E50]`}>
-              Latest blog posts
-            </h1>
-            {posts.map((post, idx) => (
-              <PostTeaser key={idx} {...post} />
-            ))}
-          </>
-        )}
+        <>
+          <h1
+            className={`${amaticSC.className} mb-8 text-center font-bold text-4xl text-[#FF4E50]`}>
+            Latest blog posts
+          </h1>
+          {posts.map((post, idx) => (
+            <PostTeaser key={idx} {...post} />
+          ))}
+        </>
       </div>
     </main>
   )
